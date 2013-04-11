0450f51dbf0e3943ef63da0527edc4e01bf557aa
Davis Silverman <sinistersnare@gmail.com>
2013-04-10 23:10:02 -0400

    fixed an issue with the last commit
    also cleaned up some more, added a new operator (/) which prints the
    value of the current cell. Git fucked up the code last time, lets see if
    this happens again!!!! (but hope it doesnt)...

--- PyBrainfuck/pybf/pybf.py
+++ PyBrainfuck/pybf/pybf.py
@@ -3,23 +3,16 @@
 """
 brainfuck interpreter written in python
 @author: Davis-Sinistersnare
-<<<<<<< HEAD
-=======
 @version 1.1
->>>>>>> Fixed indenting mishaps, started expanding.
 """
 
 ####################################################
 """
 TODO:
-<<<<<<< HEAD
--have REPL remember its history DONE
--give REPL a '_' feature, like the Python REPL
--CLASSIFY DONE (mostly)
-=======
 -There is a semantic error in the makemap method. does not catch the KeyboardError (or any error) if mismatching brackets appear
 -I HAVE NO IDEA!
->>>>>>> Fixed indenting mishaps, started expanding.
+-IDEA! i could add unit tests! what a good idea! good job sinistersnare!
+-Its too late...time for bed!
 """
 ####################################################
 
@@ -34,15 +27,14 @@ class BFE:
    """
 
    def __init__(self,code='',repl=True):
-
       """ initializes all of the fields for use in the class """
-      self.operators = ['+','-','<','>',',','.','[',']']
+      
+      self.operators = ['+','-','<','>',',','.','[',']','/']
       self.code = filter(lambda x: x in self.operators, code)
       self.cells = [0]
       self.pos = 0
       self.codepos = 0
       self.prompt = "bf> "
-      #self.bmap = makemap() #might be an error here, lets see...
 
    def makemap(self):
       """ builds a map for the loop to run through """
@@ -52,161 +44,46 @@ class BFE:
       #THIS DOESNT WORK
       try:
          for pos, val in enumerate(self.code):
-         
+        
             if val == '[':
                lbmap.append(pos)
             if val == ']':
-<<<<<<< HEAD
-                popd = lbmap.pop()
-                bmap[popd] = pos
-                bmap[pos] = popd
-        return bmap
-    #end _makemap()
-    
-    def execute(self,repl=True,to_use=''):
-        """
-        runs through the code and executes it
-        """
-        #doing this so i dont have to write out `self` each time
-        #woohoo i understand scopes :D
-        if not to_use:
-            code = self.code
-        else:
-            code = to_use
-        codepos = self.codepos
-        cells = self.cells
-        pos = self.pos
-        bmap = self.makemap()
-        prevcode = ""
-        while codepos < len(code):
-            token = code[codepos]
-        
-            if token == '+':
-                cells[pos] += 1
-                
-            elif token == '-':
-                cells[pos] -= 1
-                
-            elif token == '>':
-                pos += 1
-                if pos >= len(cells):
-                    cells.append(0)
-
-            elif token == '<':
-                pos = 0 if pos <= 0 else pos - 1
-                
-            elif token == '.':
-                sys.stdout.write(chr(cells[pos]))
-                
-            elif token == ',':
-                try:
-                    cells[pos] = ord(raw_input()[0])
-                except IndexError:
-                    cells[pos] = 0
-                    
-            elif token == '[' and cells[pos] == 0:
-                codepos = bmap[codepos]
-            
-            elif token == ']' and cells[pos] != 0:
-                codepos = bmap[codepos]
-            
-            elif repl and code == '_':
-                #dont need to check token if the entire code is just '_'
-                self.execute(to_use=prevcode)
-                
-            else:
-                pass  # anything else doesnt matter
-            
-            codepos += 1
-        if code != '_':
-            prevcode = code
-            code = ""
-        print
-        return
-
-    def repl(self):
-        """
-        the standard REPL for my bf interpreter, missing many key features (see TODO section)
-        """
-        while True:
-            self.code = raw_input(self.prompt)
-            self.execute()
-    #end repl()
-                
-#end class BFE
-
-def main(code="",repl=True,cmd=""):
-    """
-    starts the execution of code
-    """
-    
-    bfe = BFE()
-    if repl:
-        if cmd:
-            bfe.prompt = cmd
-        bfe.repl()
-    elif code:
-        bfe.code=code
-        bfe.execute(repl=False)
-        
-        
-    print
-    
-    
-    
-if __name__ == '__main__': 
-    parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-f","--file",help="Specifies a file for use.")
-    group.add_argument("-r","--repl",help="uses a repl. argument will be prompt empty uses default")
-    
-    args = parser.parse_args()
-    
-    if args.file:
-        main(code=open(args.file).read(),repl=False)
-    elif args.repl:
-        main(prompt=args.repl)
-    else:
-        main()
-    
-    
-=======
                popd = lbmap.pop()
                bmap[popd] = pos
                bmap[pos] = popd
       except:
-         print "WATWAT"
-         sys.stdout.write("Missing a matching brace! tricky tricky!")
-         pass
-               
-            
-       
+         sys.stderr.write("Bracket Mismatch!")
+      
       return bmap
-
+    #end _makemap()
+    
    def execute(self,repl=True,to_use=''):
-      """ runs through the code and executes it token by token"""
-
+      """
+      runs through the code and executes it
+      """
       #doing this so i dont have to write out `self` each time
       #woohoo i understand scopes :D
+      
       if not to_use:
          code = self.code
       else:
          code = to_use
+         
       codepos = self.codepos
       cells = self.cells
       pos = self.pos
       bmap = self.makemap()
       prevcode = ""
-
+      
       while codepos < len(code):
          token = code[codepos]
-
+     
          if token == '+':
             cells[pos] += 1
-
+               
          elif token == '-':
             cells[pos] -= 1
-
+               
          elif token == '>':
             pos += 1
             if pos >= len(cells):
@@ -214,31 +91,29 @@ if __name__ == '__main__':
 
          elif token == '<':
             pos = 0 if pos <= 0 else pos - 1
-            print pos
-
+                
          elif token == '.':
-            try:
-               sys.stdout.write(chr(cells[pos]))
-            except ValueError:
-               print("An error has occured! You seem to have gone out of bounds! Destroying the program now..."),
-               pass
-
+            sys.stdout.write(chr(cells[pos]))
+                
          elif token == ',':
             try:
                cells[pos] = ord(raw_input()[0])
             except IndexError:
                cells[pos] = 0
-
+                    
          elif token == '[' and cells[pos] == 0:
             codepos = bmap[codepos]
-
+            
          elif token == ']' and cells[pos] != 0:
             codepos = bmap[codepos]
+            
+         elif token == '/':
+            sys.stdout.write(str(cells[pos]))            
+            
          else:
             pass  # anything else doesnt matter
-
+            
          codepos += 1
-        
       print
       return
 
@@ -249,61 +124,41 @@ if __name__ == '__main__':
       while True:
          self.code = raw_input(self.prompt)
          self.execute()
+   #end repl()
+                
+#end class BFE
 
-          
-def main(code='',repl=True,cmd=''):
+def main(code="",repl=True,cmd=""):
    """
-   starts the execution of the code
+   starts the execution of code
    """
-
+    
    bfe = BFE()
    if repl:
       if cmd:
          bfe.prompt = cmd
       bfe.repl()
    elif code:
-      bfe.code = code
+      bfe.code=code
       bfe.execute(repl=False)
+        
+        
    print
-
-
-if __name__ == '__main__':
-   parser = argparse.ArgumentParser(prog="PyBrainfuck",version="1.1")
-
+    
+    
+    
+if __name__ == '__main__': 
+   parser = argparse.ArgumentParser()
    group = parser.add_mutually_exclusive_group()
-
    group.add_argument("-f","--file",help="Specifies a file for use.")
    group.add_argument("-r","--repl",help="uses a repl. argument will be prompt empty uses default")
-
+    
    args = parser.parse_args()
-
+    
    if args.file:
       main(code=open(args.file).read(),repl=False)
    elif args.repl:
-      print args.repl
-
-      main(prompt=args.r)
-
+      main(prompt=args.repl)
    else:
       main()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> Fixed indenting mishaps, started expanding.
