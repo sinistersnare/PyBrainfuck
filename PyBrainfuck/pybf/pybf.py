622459275f724568e00709990ad79f1d3e89d05f
Davis Silverman <sinistersnare@gmail.com>
2013-04-12 01:23:37 -0400

    commiting a 100% borked build. good stuff though
    if it werent for that darn key error, this would be amazing!!!!
    
    ill fix this when it isnt 1:30 AM, and i dont have school in the
    morning...
    
    i refactored the execute method into multiple sub methods, dont know how
    good of an idea this is, but we'll see!

--- PyBrainfuck/pybf/pybf.py
+++ PyBrainfuck/pybf/pybf.py
@@ -9,6 +9,7 @@ brainfuck interpreter written in python
 ####################################################
 """
 TODO:
+-pretty sure this is 100% broken, but am pushing it to mark a moment of progress!
 -Catching an error when you increment a cell, then attempt to throw with unmatched braces...
 -The above is really pissing me off, but ill push anyways.
 -I HAVE NO IDEA!
@@ -18,7 +19,7 @@ TODO:
 ####################################################
 
 
-
+#import this,antigravity
 import sys
 import argparse
 
@@ -30,21 +31,20 @@ class BFE:
 
    def __init__(self,code='',repl=True):
       """ initializes all of the fields for use in the class """
-      
+      self.mapping = {'+':self._add, '-':self._sub, '<':self._inc, '>':self._dec, ',':self._input, '.':self._out, '[':self._lbrace, ']':self._rbrace, '/':self._cellval}
       self.operators = ['+','-','<','>',',','.','[',']','/']
       self.code = filter(lambda x: x in self.operators, code)
       self.cells = [0]
       self.pos = 0
       self.codepos = 0
       self.prompt = "bf> "
-
+      self.endof = ''
+      self.bmap = self.makemap()
    def makemap(self):
       """ builds a map for the loop to run through """
+      
       bmap = {}
       lbmap = []
-      
-      #THIS DOESNT WORK
-      
       
       for pos, val in enumerate(self.code):
          if val == '[':
@@ -52,7 +52,6 @@ class BFE:
             
          if val == ']':
             try:
-            
                popd = lbmap.pop()
                bmap[popd] = pos
                bmap[pos] = popd
@@ -64,75 +63,123 @@ class BFE:
       return bmap
     #end _makemap()
     
+   def _add(self):
+      """adds one to the current cell"""
+      
+      cells = self.cells
+      pos = self.pos
+      
+      if not cells[pos] >= 255: cells[pos] += 1
+      
+   
+   def _sub(self):
+      """subtracts one from the current cell"""
+      
+      cells = self.cells
+      pos = self.pos
+      
+      if not cells[pos] <= 0: cells[pos] -= 1
+
+   
+   def _inc(self):
+      """Increments the pointer position. Will add a 0 to the end of the list if it is maximized"""
+      
+      pos = self.pos
+   
+      pos += 1
+      if pos >= len(cells): cells.append(0)
+   
+   def _dec(self):
+      """Decrements the pointer position. will retain at 0 if already there"""
+      
+      pos = self.pos
+      
+      if not pos != 0: pos -= 1
+   
+   def _out(self):
+      """outputs the ASCII value of the value at the position"""
+      
+      cells = self.cells
+      pos = self.pos
+      
+      sys.stdout.write(chr(cells[pos]))
+               
+   def _input(self):
+      """Reads from stdin, then writes the int value from ASCII into the grid"""
+      
+      cells = self.cells
+      pos = self.pos
+      try:
+         rinput = raw_input()[0]
+         ordinput = ord(rinput)
+         if 0 < ordinput < 31: cells[pos] = 0 #0-31 are all null-like characters in ASCII
+         else: cells[pos] = ordinput #if its not null-like, than put it into the cells
+      except IndexError:
+         cells[pos] = 0
+ 
+   def _lbrace(self):
+      """If the value of the current cell is 0, then go to the mapped right brace, otherwise, do nothing (go throught the loop) """
+      cells = self.cells
+      pos = self.pos
+      bmap = self.bmap
+      codepos = self.codepos
+         
+      if not cells[pos]: #is 0
+         try:
+            codepos = bmap[codepos]
+         except KeyError:
+            sys.stderr.write("BracketingError! \"[\" python KeyError")
+                          
+   def _rbrace(self):
+      """If the value of the current cells is NOT zero, then it goes back to the mapped left brace to re-loop """
+      cells = self.cells
+      pos = self.pos
+      bmap = self.bmap
+      codepos = self.codepos
+      
+      if cells[pos]: #not 0
+         try:
+            codepos = bmap[codepos]
+         except KeyError:
+            #happens when there is a cell thats not 0
+            sys.stderr.write("BracketingError! \"]\" python KeyError")            
+
+   def _cellval(self):
+      """My own little operator! This just prints the int value of the current cell """
+      cells = self.cells
+      pos = self.pos
+      
+      sys.stdout.write(str(cells[pos]))
+   
+
    def execute(self,repl=True,to_use=''):
       """
-      runs through the code and executes it
+      runs throught the code and executes it
       """
-      #doing this so i dont have to write out `self` each time
-      #woohoo i understand scopes :D
       
       if not to_use:
          code = self.code
       else:
          code = to_use
-         
+      
       codepos = self.codepos
       cells = self.cells
       pos = self.pos
-      bmap = self.makemap()
-      prevcode = ""
+      bmap = self.bmap
+      funcs = self.mapping
       
       while codepos < len(code):
          token = code[codepos]
-     
-         if token == '+':
-            cells[pos] += 1
-               
-         elif token == '-':
-            cells[pos] -= 1
-               
-         elif token == '>':
-            pos += 1
-            if pos >= len(cells):
-               cells.append(0)
-
-         elif token == '<':
-            pos = 0 if pos <= 0 else pos - 1
-                
-         elif token == '.':
-            sys.stdout.write(chr(cells[pos]))
-                
-         elif token == ',':
-            try:
-               cells[pos] = ord(raw_input()[0])
-            except IndexError:
-               cells[pos] = 0
-                    
-         elif token == '[' and cells[pos] == 0:
-            try:
-               codepos = bmap[codepos]
-            except KeyError:
-               sys.stderr.write("BracketingError! \"[\" python KeyError")
-               
-            
-         elif token == ']' and cells[pos] != 0:
-            try:
-               codepos = bmap[codepos]
-            except KeyError:
-               #pretty certain this will never happen!
-               sys.stderr.write("BracketingError! \"]\" python KeyError")
-               
-               
-         elif token == '/':
-            sys.stdout.write(str(cells[pos]))            
-            
-         else:
-            pass  # anything else doesnt matter
-            
+         
+         if token not in funcs: print "TOKEN IS NOT IN FUNCS. WAT" #just in case
+         funcs[token]()
+         
          codepos += 1
+         
       print
       return
-
+         
+   
    def repl(self):
       """
       the standard REPL for my bf interpreter, missing many key features (see TODO section)
